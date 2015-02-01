a25e110d8b88add9d0ba6a337de1488a96d16de1
Nagyhal <nagyhal@zoho.com>
2015-02-01 23:34:05 +0000

    Add Beast Heart: Feast of Blood

--- data/talents/mounted/beast-heart.lua
+++ data/talents/mounted/beast-heart.lua
@@ -1,7 +1,53 @@
 newTalent{
-	name = "Gruesome Depredation",
+	name = "Feast of Blood",
 	type = {"mounted/beast-heart", 1},
 	require = mnt_wil_req1,
+	points = 5,
+	cooldown = function(self, t) return self:combatTalentLimit(t, 15, 35, 25) end,
+	loyalty = 20,
+	tactical = { HEAL = 2 }, --TODO: Complicated AI routine
+	on_pre_use = function(self, t, silent)
+		if self:isMounted() then
+			if not silent then
+				game.logPlayer(self, "You must be dismounted and adjacent to your beast!")
+			end
+			return false
+		end
+		local mount=self:hasMount()
+		if not mount or core.fov.distance(self.x, self.y, mount.x, mount.y)~=1 then
+			if not silent then
+				game.logPlayer(self, "You must be dismounted and adjacent to your beast!")
+			end
+			return false
+		end	
+		return true
+	end, 
+	action = function(self, t)
+		local mount=self:hasMount()
+		mount:setEffect(mount.EFF_CUT, 6, {power=t.getBleed(self, t)/6})
+		self:heal(t.getHeal(self, t))
+		self:incStamina(t.getStamina(self, t))
+		return true
+	end,
+	info = function(self, t)
+		local heal = t.getHeal(self, t)
+		local stamina = t.getStamina(self, t)
+		local bleed_per = t.getBleed(self, t)/6
+		return ([[Partaking of your beastial mount's unfaltering devotion through times of greatest need, you quench your hunger by loosing a little of its blood. Must be dismounted and adjacent to the beast. Regain %d health and %d stamina, while inflicting 6 turns of bleeding for %d damage upon your beast.]]):
+			format(heal, stamina, bleed_per)
+	end,
+	getStamina = function(self, t) return self:combatTalentScale(t, 10, 60) end,
+	getHeal = function(self, t) return self:combatTalentScale(t, 120, 300) end,
+	getBleed = function(self, t)
+			local heal = t.getHeal(self, t)
+			return heal / self:combatTalentScale(t, 5, 6)
+	end,
+}
+
+newTalent{
+	name = "Gruesome Depredation",
+	type = {"mounted/beast-heart", 2},
+	require = mnt_wil_req2,
 	points = 5,
 	cooldown = 30,
 	tactical = { ATTACK = 2 }, --TODO: Complicated AI routine
